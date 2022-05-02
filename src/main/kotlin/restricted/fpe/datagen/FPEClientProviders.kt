220ffed3a5cd7ba1b2d5bb18a91f6304b06214b2
Taskeren <nitu2003@126.com>
2022-05-02 21:23:23 +0800

    添加语言框架

--- src/main/kotlin/restricted/fpe/datagen/FPEClientProviders.kt
+++ src/main/kotlin/restricted/fpe/datagen/FPEClientProviders.kt
@@ -1,5 +1,16 @@
 package restricted.fpe.datagen
 
+import net.minecraft.data.DataGenerator
+import net.minecraft.world.effect.MobEffect
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.enchantment.Enchantment
+import net.minecraft.world.level.block.Block
+import net.minecraftforge.common.data.LanguageProvider
+import restricted.fpe.ModId
+import java.util.function.Supplier
+
 /*
 import net.minecraft.data.DataGenerator
 import net.minecraftforge.client.model.generators.BlockStateProvider
@@ -17,3 +28,65 @@ internal class SFItemModelProvider(g: DataGenerator, h: ExistingFileHelper) : It
 	}
 }
 */
+
+internal class FPELanguageProvider(dataGen: DataGenerator, language: String, val block: (FPELanguageProvider) -> Unit) :
+	LanguageProvider(dataGen, ModId, language) {
+	override fun addTranslations() {
+		block(this)
+	}
+}
+
+internal object FPELanguage {
+
+	private val entries = mutableListOf<LanguageEntry>()
+
+	fun buildEnglish(provider: FPELanguageProvider) {
+		entries.forEach {
+			provider.add(it.key, it.en ?: it.key)
+		}
+	}
+
+	fun buildChinese(provider: FPELanguageProvider) {
+		entries.forEach {
+			provider.add(it.key, it.zh ?: it.key)
+		}
+	}
+
+	fun buildTraditionalChinese(provider: FPELanguageProvider) {
+		entries.forEach {
+			provider.add(it.key, it.zhTw ?: it.key)
+		}
+	}
+
+	fun add(key: String, func: LanguageEntry.() -> Unit) {
+		entries += LanguageEntry(key).apply(func)
+	}
+
+	fun item(item: Item, func: LanguageEntry.() -> Unit) {
+		entries += LanguageEntry(item.descriptionId).apply(func)
+	}
+
+	fun block(block: Block, func: LanguageEntry.() -> Unit) {
+		entries += LanguageEntry(block.descriptionId).apply(func)
+	}
+
+}
+
+internal class LanguageEntry(val key: String) {
+
+	internal var en: String? = null
+		private set
+	internal var zh: String? = null
+		private set
+	internal var zhTw: String? = null
+		private set
+
+	fun en(english: String) = apply { en = english }
+
+	fun zh(chinese: String) = apply {
+		zh = chinese
+		if(zhTw == null) zhTw = chinese
+	}
+
+	fun tw(traditionalChinese: String) = apply { zhTw = traditionalChinese }
+}
--- src/main/kotlin/restricted/fpe/datagen/FPEDataGenerator.kt
+++ src/main/kotlin/restricted/fpe/datagen/FPEDataGenerator.kt
@@ -5,7 +5,40 @@ package restricted.fpe.datagen
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent
+import restricted.fpe.FPE
+import restricted.fpe.ModId
 
 @SubscribeEvent
 fun gatherData(e: GatherDataEvent) {
+	if(e.includeClient()) {
+		val generator = e.generator
+
+		registerLanguages()
+
+		generator.addProvider(FPELanguageProvider(generator, "en_us", FPELanguage::buildEnglish))
+		generator.addProvider(FPELanguageProvider(generator, "zh_cn", FPELanguage::buildChinese))
+		generator.addProvider(FPELanguageProvider(generator, "zh_tw", FPELanguage::buildTraditionalChinese))
+	}
+}
+
+private fun registerLanguages() = FPELanguage.apply {
+
+	add("itemGroup.$ModId") {
+		en("Fire Protection Equipment")
+		zh("想不出来")
+		tw("想不出來")
+		// TODO: 把[想不出来]给换了
+	}
+
+	block(FPE.Blocks.FireHydrant) {
+		en("Fire Hydrant")
+		zh("消防栓")
+		tw("消防栓")
+	}
+
+	item(FPE.Items.Fire) {
+		en("FPE")
+		zh("消防设备")
+		tw("消防設施")
+	}
 }
