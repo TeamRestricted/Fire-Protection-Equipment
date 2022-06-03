891777c2ebdebb77203d89ef000bfc1e010a2e10
Taskeren <nitu2003@126.com>
2022-06-03 21:39:16 +0800

    初步完成消防喷头具体功能

--- src/main/kotlin/restricted/fpe/item/HoseNozzleItem.kt
+++ src/main/kotlin/restricted/fpe/item/HoseNozzleItem.kt
@@ -1,7 +1,52 @@
 package restricted.fpe.item
 
+import net.minecraft.core.particles.ParticleOptions
+import net.minecraft.core.particles.ParticleTypes
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
-import restricted.fpe.FPEConst
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import restricted.fpe.*
+import restricted.fpe.util.ExtinguishRayTracer
 
 object HoseNozzleItem : Item(FPEConst.ItemConst.DefaultNonStackableItemProp) {
+
+	override fun getUseDuration(pStack: ItemStack): Int = 7200
+
+	override fun use(pLevel: Level, pPlayer: Player, pUsedHand: InteractionHand): InteractionResultHolder<ItemStack> {
+		pPlayer.startUsingItem(pUsedHand)
+		return InteractionResultHolder.consume(pPlayer.getItemInHand(pUsedHand))
+	}
+
+	override fun onUsingTick(stack: ItemStack, player: LivingEntity, count: Int) {
+		player.level.runOnRemote {
+			val tracer = ExtinguishRayTracer(player)
+			when(count % 2) {
+				0 -> handleHit(player, tracer.hitEntity)
+				else -> addParticles(this, player, tracer, ParticleTypes.FLAME)
+			}
+		}
+	}
+
+	private fun addParticles(level: ServerLevel, player: LivingEntity, tracer: ExtinguishRayTracer, particle: ParticleOptions) {
+		val offX = player.getUpVector(1.0F).cross(player.lookAngle)
+		val offFactor = if(player.usedItemHand == InteractionHand.MAIN_HAND) -0.4 else 0.4
+		val discretized = tracer.discretized(64)
+		val discretizedSize = discretized.size
+		for(index in 0 until discretizedSize) {
+			val pos = discretized[index].add(offX.scale(index * offFactor / discretizedSize))
+			level.sendParticles(particle, pos, 1, 0.0)
+		}
+	}
+
+	private fun handleHit(player: LivingEntity, target: Entity?) {
+		target?.hurt(DamageSource.DRY_OUT, 1.0F)
+	}
+
 }
---
+++ src/main/kotlin/restricted/fpe/util/ExtinguishRayTracer.kt
@@ -0,0 +1,95 @@
+package restricted.fpe.util
+
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.projectile.ProjectileUtil
+import net.minecraft.world.level.ClipContext
+import net.minecraft.world.phys.*
+import java.util.*
+import kotlin.math.floor
+import kotlin.math.max
+
+/**
+ * 这个类改写自土球老师的 WaterSprayer 的 SrayerRayTracer，
+ * 依照开源协议 WTFPL v2 抄下来的。
+ *
+ * https://github.com/ustc-zzzz/WaterSprayer/blob/master/src/main/scala/com/github/ustc_zzzz/watersprayer/sprayer/SprayerRayTracer.scala
+ */
+class ExtinguishRayTracer(val entity: LivingEntity) {
+
+	data class Traced(val motion: Vec3, val pos: Vec3)
+
+	private var hit: HitResult? = null
+
+	private val traced: List<Traced> by lazy {
+		val startPos = entity.position().add(0.0, entity.eyeHeight * 0.6, 0.0)
+		val startMotion = entity.lookAngle.add(0.0, 0.2, 0.0).normalize()
+
+		val tracedList = LinkedList<Traced>().apply { add(Traced(startMotion, startPos)) }
+
+		var continueRayTracing = true
+		while(continueRayTracing) {
+			val motion = tracedList[0].motion
+			val pos = tracedList[0].pos
+
+			val newMotion = motion.subtract(0.0, 0.1, 0.0).run {
+				if(this.lengthSqr() <= 1) this else this.normalize()
+			}
+			var newPos = pos.add(motion).add(newMotion)
+
+			val blockResult = entity.level.clip(ClipContext(pos,  newPos, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, entity))
+			if(blockResult.type != HitResult.Type.MISS) { // on hit block
+				hit = blockResult
+				continueRayTracing = false
+				newPos = blockResult.location
+			}
+
+			val bb1 = AABB(pos, newPos).inflate(1.0)
+			val entityResult = ProjectileUtil.getEntityHitResult(entity.level, entity, pos, newPos, bb1) {
+				!it.isSpectator && it.isPickable && it != entity
+			}
+			if(entityResult != null) { // on hit entity
+				hit = entityResult
+				continueRayTracing = false
+				val bb2 = entityResult.entity.boundingBox.inflate(0.3)
+				val bboxMaxSize = max(bb2.xsize, max(bb2.ysize, bb2.zsize))
+				val endPos = pos.add(newPos.subtract(pos).run { this.normalize().scale(bboxMaxSize + this.length()) })
+				newPos = bb2.clip(endPos, pos).orElse(newPos).add(bb2.clip(pos, endPos).orElse(newPos)).scale(0.5)
+			}
+			tracedList.push(Traced(newMotion, newPos))
+			if(newPos.y < 0) continueRayTracing = false
+		}
+
+		tracedList
+	}
+
+	val hitEntity: Entity? get() = if(hit is EntityHitResult) (hit as EntityHitResult).entity else null
+
+	fun discretized(factor: Int): List<Vec3> {
+		val tmpTraced = LinkedList(traced)
+		val head = tmpTraced.removeFirst()
+		var prevPos = head.pos
+		var prevMotion = head.motion
+
+		val bd = LinkedList<Vec3>()
+
+		for(nextTraced in tmpTraced) {
+			val nextPos = nextTraced.pos
+			val nextMotion = nextTraced.motion
+			val max = factor * nextPos.subtract(prevPos).length() / nextMotion.add(prevMotion).length()
+			for(i in floor(max).toInt() downTo 1) {
+				val alpha = i / max
+				val beta = (max - i) / max
+				val alphaPos = nextPos.add(nextMotion.scale(alpha * max / factor))
+				val betaPos = prevPos.subtract(prevMotion.scale(beta * max / factor))
+				bd += betaPos.scale(alpha).add(alphaPos.scale(beta))
+			}
+			prevPos = nextPos
+			prevMotion = nextMotion
+		}
+
+		bd += prevPos
+		return bd
+	}
+
+}
